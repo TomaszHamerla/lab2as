55b5f28e7a8fc933877290588ed660938bc9eb85
unknown <tomaszhamerla@interia.pl>
2024-11-09 16:18:04 +0100

    lab 2 exercise

--- app/src/main/java/com/example/lab2as/MainActivity.java
+++ app/src/main/java/com/example/lab2as/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.lab2as;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText ilePrzeliczyc;
+    private Button buttonUsd, buttonEur, buttonChf;
+    private TextView wynik;
+    private final double USD_TO_PLN = 3.99;
+    private final double EUR_TO_PLN = 4.32;
+    private final double CHF_TO_PLN = 4.53;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        buttonUsd = findViewById(R.id.buttonUsd);
+        buttonEur = findViewById(R.id.buttonEur);
+        buttonChf = findViewById(R.id.buttonChf);
+
+        buttonUsd.setOnClickListener(v -> countResult(USD_TO_PLN));
+        buttonEur.setOnClickListener(v -> countResult(EUR_TO_PLN));
+        buttonChf.setOnClickListener(v -> countResult(CHF_TO_PLN));
+    }
+
+    private void countResult(double currency) {
+        ilePrzeliczyc = findViewById(R.id.ilePrzeliczyc);
+        String string = ilePrzeliczyc.getText().toString();
+        double amountToConvert  = Double.parseDouble(string);
+
+       double result = amountToConvert * currency;
+
+        wynik = findViewById(R.id.wynik);
+        wynik.setText(String.valueOf(result));
     }
 }
